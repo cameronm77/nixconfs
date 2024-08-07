b461ec0e9f85fd794ce7a71c6cb59a89f1f845e8
lookismashed <156612654+cameronm77@users.noreply.github.com>
2024-08-06 19:49:26 -0500

    Update services.nix

--- NixOS/etc/nixos/services.nix
+++ NixOS/etc/nixos/services.nix
@@ -21,14 +21,14 @@
 
     security.pam.services.login.enableKwallet = true;
 
-    #services.udev.extraRules = ''
-    #     ACTION=="remove",\
-    #      ENV{ID_BUS}=="usb",\
-    #      ENV{ID_MODEL_ID}=="0407",\
-    #      ENV{ID_VENDOR_ID}=="1050",\
-    #      ENV{ID_VENDOR}=="Yubico",\
-    #      RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
-    #  '';
+    services.udev.extraRules = ''
+         ACTION=="remove",\
+          ENV{ID_BUS}=="usb",\
+          ENV{ID_MODEL_ID}=="0407",\
+          ENV{ID_VENDOR_ID}=="1050",\
+          ENV{ID_VENDOR}=="Yubico",\
+          RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+     '';
     #security.duosec = {
       #ssh.enable = true;
       #pam.enable = true;
